6ab726fd21a1a5c354b4aed0b59f5e6e56fa20ab
Jonsonice <415410795@qq.com>
2018-12-26 17:58:33 +0800

    vuex中央状态管理

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <product-list-one :products="products"></product-list-one>
+    <product-list-two :products="products"> </product-list-two>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import ProductListOne from './components/ProductListOne.vue'
+import ProductListTwo from './components/ProductListTwo.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    'product-list-one':ProductListOne,
+    'product-list-two':ProductListTwo
+  },
+  data(){
+    return {
+      
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  font-family: Ubuntu;
+  color: #555;
 }
 </style>
---
+++ src/store/store.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+  state:{
+    products:[
+        {name:"陈厂长",price:4000},
+        {name:"陈拓海",price:2000},
+        {name:"陈地主",price:1400},
+        {name:"陈抠门",price:1000}
+      ]
+  }
+})
